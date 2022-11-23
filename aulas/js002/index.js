4b6e08e715944f1a1807c0205aac28eaf4b020c9
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2022-11-23 16:28:27 +0100

    Primeiros exercícios  JS

---
+++ aulas/js002/index.js
@@ -0,0 +1,7 @@
+document.getElementById("text").innerHTML ="Primeiro texto escrito com <strong>JS</strong>";
+
+alert('Hello, be welcome site of Otaniel Fernandes');
+alert(10 + 10);
+
+console.log(10 * 10);
+console.log('It was writed with: console.log');
