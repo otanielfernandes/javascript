598cd5665f69998858a69856c75295fe9fb6c276
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2022-12-04 16:48:03 +0100

    Exercícios de Java Script

--- aulas/js010/index.js
+++ aulas/js010/index.js
@@ -1,6 +1,6 @@
 // Criando uma fábrica (função):
 
-function Clique(){
+ function Clique(){
     // Quando eu clicar uma vez no botão ele abrirá uma pequena janela dizendo o que está escrito abaixo;
 
     alert("Olá, você clicou no botão")
@@ -11,6 +11,40 @@ function DuploClique(){
     document.body.style.backgroundColor = "BlueViolet";
 }
 
-function MouseCima(){
-    document.style.div.backgroundcolor="yellow";
+function viraAmarelo(){
+    // Qunado passar o mouse dentro do elemento e o mesmo mudar de cor
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "yellow";
+}
+
+function viraRoxo(){
+    // Quando o mouse passar fora do elemento, o elemento deve voltar a cor inicial
+    let div = document.getElementById("teste");
+    div.style.backgroundColor = "purple";
+}
+
+/* function adicionaTexto(){
+    let p = document.getElementById("texto");
+    p.append('Otaniel Fernandes');
+    p.style.color = "red";
+}
+*/
+
+function precionarBotao(){
+    alert('Você pressionou o botão e não soltou ainda');
+}
+
+function Focus(){
+    let input = document.getElementById("campoTexto");
+
+    input.style.backgroundColor = "yellow";
+}
+
+function soltarBotao(){
+    alert('Opah, agora você soltou o botão');
+}
+
+function Tecla(){
+    let input = document.getElementById("campoTexto").value;
+    console.log(input);
 }
