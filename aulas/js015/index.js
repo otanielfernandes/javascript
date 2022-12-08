3f483ee581608022f840069abfb8f8f8c396bd79
Otaniel Fernandes <113445501+otanielfernandes@users.noreply.github.com>
2022-12-08 21:39:43 +0100

    New

--- aulas/js015/index.js
+++ aulas/js015/index.js
@@ -1,19 +1,52 @@
-function verificaCor(){
+//Criando uma função:
+
+
+function pegaCor(){
     let cor = document.getElementById("icor").value;
+    cor = cor.toLowerCase();
+
     switch(cor){
-        case "Azul":
+        case "azul":
             document.body.style.backgroundColor = "blue";
         break;
-        
-        case "Amarelo":
+
+        case "amarelo":
             document.body.style.backgroundColor = "yellow";
         break;
 
-        case "Roxo":
+        case "vermelho":
+            document.body.style.backgroundColor = "red";
+        break;
+
+        case "roxo":
             document.body.style.backgroundColor = "purple";
-            break;
+        break;
+
+        case "laranja":
+            document.body.style.backgroundColor = "orange";
+        break;
+
+        case "preto":
+            document.body.style.backgroundColor = "black";
+        break;
+
+        case "cinzento":
+            document.body.style.backgroundColor = "gray";
+        break;
+
+        case "verde":
+            document.body.style.backgroundColor = "green";
+        break;
+
+        case "castanho":
+            document.body.style.backgroundColor = "#351601";
+        break;
+
+        case "rosa":
+            document.body.style.backgroundColor = "pink";
+        break;
 
         default:
-            document.getElementById("teste").innerHTML = "A cor " +  cor + " "  + "encontra-se indisponível no nosso site";
+            document.getElementById("teste").innerHTML = "Cor indisponível:" +  " " +cor;
     }
 }
